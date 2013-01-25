57e5c08379a2fb9675af4c2fb41a7a3f53686ff7
Marcelo Eden <edendroid@gmail.com>
2013-01-25 18:51:19 -0300

    do not change the hash keys

--- lib/dot_hash/properties.rb
+++ lib/dot_hash/properties.rb
@@ -8,26 +8,25 @@ module DotHash
     end
 
     def method_missing(key, *args, &block)
-      symbolize_key key
       has_key?(key) ?
         execute(key, *args, &block) :
         super(key, *args, &block)
     end
 
     def respond_to?(key)
-      symbolize_key key
       has_key?(key) or super(key)
     end
 
     def [](key)
-      symbolize_key key
       get_value(key)
     end
 
     private
 
     def has_key?(key)
-      hash.has_key?(key.to_sym) or hash.respond_to?(key)
+      hash.has_key?(key.to_sym) or
+        hash.has_key?(key.to_s) or
+        hash.respond_to?(key)
     end
 
     def execute(key, *args, &block)
@@ -37,15 +36,10 @@ module DotHash
     end
 
     def get_value(key)
-      key = key.to_sym
+      key = hash.has_key?(key.to_s) ? key.to_s : key.to_sym
       value = hash[key]
       return value unless value.is_a?(Hash)
       hash[key] = self.class.new value
-    end
-
-    def symbolize_key(key)
-      return unless hash.has_key?(key.to_s)
-      hash[key.to_sym] = hash.delete key.to_s
     end
   end
 end
